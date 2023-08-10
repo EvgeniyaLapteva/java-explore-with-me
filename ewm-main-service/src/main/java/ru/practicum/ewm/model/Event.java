308bd7c7a6c7953600957fa84f239e3c50bd5b8e
EvgeniyaLapteva <evgeniyalaptev4@yandex.ru>
2023-08-10 17:21:57 +0300

    add model

--- ewm-main-service/src/main/java/ru/practicum/ewm/model/Event.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/model/Event.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "events")
@@ -17,6 +18,52 @@ public class Event {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @Column
-//    private Category category;
+    @Column(name = "annotation", nullable = false)
+    private String annotation;
+
+    @OneToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "confirmed_requests")
+    private Long confirmedRequests;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDateTime eventDate;
+
+    @OneToOne
+    @JoinColumn(name = "initiator_id", nullable = false)
+    private User initiator;
+
+    @OneToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @Column(name = "paid", nullable = false)
+    private Boolean paid;
+
+    @Column(name = "participant_limit")
+    private Long participantLimit;
+
+    @Column(name = "published_on")
+    private LocalDateTime publishedOn;
+
+    @Column(name = "request_moderation")
+    private Boolean requestModeration;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false)
+    private EventState state;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "views")
+    private Long views;
 }
--- ewm-main-service/src/main/java/ru/practicum/ewm/model/Location.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/model/Location.java
@@ -4,12 +4,22 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "location")
 @Getter
 @Setter
 @ToString
 public class Location {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "lat")
     private Double lat;
 
+    @Column(name = "lon")
     private Double lon;
 }
